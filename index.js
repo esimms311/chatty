d37d36200a31e3be823e263eee826208cb216b06
esimms311 <eric.simmons26@gmail.com>
2017-01-03 16:55:12 -0700

    chatty complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+var express = require('express')
+var bodyParser = require('body-parser')
 
+var app = express();
+
+app.use(express.static('assets'))
+app.use(bodyParser.json());
+
+var messages = [
+  //  {messages:'Hey how are you?'},
+  //  {messages:'Want to go do something today?'},
+  //  {messages:'Merry Christmas ya filthy animal'}
+  // {message: 'Hey how are you?', contact: 'Ashley'},
+  // {message: 'Want to go do something today?', contact: 'Greg'},
+  // {message: 'Merry Christmas ya filthy animal', contact: 'Santa'}
+]
+
+app.get('/messages', function(req, res, next){
+  res.status(200).json({ messages: messages });
+});
+
+app.post('/messages', function(req, res, next){
+messages.push({message: req.body.message, time:new Date() });
+console.log(req.body);
+res.status(200).json({ messages: messages});
+});
+
+app.listen(3000);
